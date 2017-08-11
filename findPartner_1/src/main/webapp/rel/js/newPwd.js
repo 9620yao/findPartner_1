285fe4feb1b0ca8176fa0ea4e36901ead14c04f3
9620yao <15675456193@163.com>
2017-08-11 16:25:56 +0800

    基本上优化完成。--姚秋林

--- findPartner_1/src/main/webapp/rel/js/newPwd.js
+++ findPartner_1/src/main/webapp/rel/js/newPwd.js
@@ -1,4 +1,1 @@
-var url = window.location.href;
-var email = url.split('?')[1].split('=')[1];
-alert(email);
-$('#newemail').val(email);
+var url=window.location.href;var email=url.split("?")[1].split("=")[1];alert(email);$("#newemail").val(email);
